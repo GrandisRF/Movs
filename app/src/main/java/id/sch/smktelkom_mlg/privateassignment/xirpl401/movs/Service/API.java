f42808d98c9f27a66a2ea114465f4566f903ec67
Ahmad Dimas Abid M <ahmaddimas0@gmail.com>
2017-05-14 22:14:29 +0700

    Add API

---
+++ app/src/main/java/id/sch/smktelkom_mlg/privateassignment/xirpl401/movs/Service/API.java
@@ -0,0 +1,6 @@
+package id.sch.smktelkom_mlg.privateassignment.xirpl401.movs.Service;
+
+public class API {
+    static public String API_URL = "http://api.themoviedb.org/3/discover/movie";
+    static public String API_KEY = "f2f590fcd2fdf04c6bf809b6c08b3ef9";
+}
